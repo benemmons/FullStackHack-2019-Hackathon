326b8c070679c6b6b10f6dbec0022c1207d7e093
Ben Emmons <ben@emmons.biz>
2019-02-16 16:19:42 +0000

    added ability to calculate distance between two countries

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const distance = require('google-distance');
 
 //Route setup
-app.get('/', (req, res) => {
+app.get('/getCountry', (req, res) => {
   
+
+
+
+  });
   res.send('root route');
 
-  
+app.get("/calculateDistance", (req, res) => {
+  distance.get(
+    {
+      origin: req.query.origin,
+      destination: req.query.destination
+    },
+    function(err, data) {
+      if (err) return console.log(err);
+      res.send(data);
+  });
 })
 
 //Start server
@@ -15,4 +29,4 @@ app.listen(port, (req, res) => {
 
 console.log(`server listening on port: ${port}`)
 
- });
+});
